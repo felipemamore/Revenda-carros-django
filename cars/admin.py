5a3141d9d008cc14fecbbda9fac1fd05e7f14806
felipemamore <felipemamore@gmail.com>
2024-01-22 22:11:26 -0300

    subindo os primeiros passos do projeto

---
+++ cars/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from cars.models import Car
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand' )
+
+admin.site.register(Car, CarAdmin)
---
+++ cars/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.model
+
+
+
